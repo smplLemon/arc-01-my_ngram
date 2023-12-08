94101a3ce4c06b79815b2e15f534ca5bed5ff5ce
darmanovjakhongir <darmanovjakhongir2@gmail.com>
2023-12-08 13:07:00 +0500

    My Ngram

--- my_ngram.c
+++ my_ngram.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-#define MAX_CHARACTERS 128
-
 int my_strlen(char* arr) {
     int count = 0;
     while (*arr != '\0') {
@@ -11,19 +9,18 @@ int my_strlen(char* arr) {
     return count;
 }
 
-int letters[MAX_CHARACTERS] = {0};
+int letters[128] = {0};
 
 int main(int argc, char* argv[]) {
     for (int i = 1; i < argc; i++) {
-        for (int j = 0; argv[i][j] != '\0'; j++) {
-            letters[(int)argv[i][j]]++;
-            }
-        }
-        
-    for (int i = 0; i < MAX_CHARACTERS; i++) {
-        if (letters[i] > 0) {
-            printf("%c:%d\n", (char)i, letters[i]);
+        for (int j = 0; j < my_strlen(argv[i]); j++) {
+            letters[(int) argv[i][j]]++;
         }
     }
-    return 0;
+
+    for (int i = 0; i < 128; i++) {
+        if (letters[i] > 0) {
+            printf("%c:%d\n", (char) i, letters[i]);
+        }
+    }
 }
