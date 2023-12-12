b7a66ced4ceca876480f0167ebe4553152f77a18
aabdumalikov <aabdumalikov0005@gmail.com>
2023-12-12 14:10:23 +0500

    fixed bugs

--- Ngram.c
+++ Ngram.c
@@ -1,26 +1,30 @@
-#include <stdio.h> 
-#include <string.h> 
- 
-int main(int argc, char** argv) { 
-    int count; 
-    for (char character = ' '; character <= 'z'; character++) 
-    { 
-        count = 0; 
-        int i = 1; 
-        while (i <= argc) 
-        { 
-            i++; 
-            for (int j = 0; j < strlen(argv[i]); j++) 
-            { 
-                if (character == argv[i][j]) 
-                    { 
-                        count++; 
-                    } 
-            } 
-        } 
-        if(count){ 
-            printf("%c:%d\n", character, count); 
-        } 
-    } 
-    return 0;  
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char** argv) {
+    char character = ' ';
+
+    while (character <= 'z') {
+        int count = 0;
+        int i = 1;
+
+        while (i < argc) {
+            int j = 0;
+            while (j < strlen(argv[i])) {
+                if (character == argv[i][j]) {
+                    count++;
+                }
+                j++;
+            }
+            i++;
+        }
+
+        if (count) {
+            printf("%c:%d\n", character, count);
+        }
+
+        character++;
+    }
+
+    return 0;
 }
