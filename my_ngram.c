208b873e20ee217901b790f06e6ac755c2c71686
islombek1144 <156665415+islombek1144@users.noreply.github.com>
2024-01-19 15:51:34 +0500

    Update my_ngram.c

--- my_ngram.c
+++ my_ngram.c
@@ -1,49 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define  MAX_ARRAY_SIZE 128
-int ismoil(char *str) {
-    int ismoil01 = 0;
 
-    while (str[ismoil01] != '\0') {
-        ismoil01 += 1;
+#include <stdio.h> 
+int main(int elment_1, char* elment_2[]) {
+    int just[128] = {0};
+    for(int i = 1; i < elment_1; i++){
+        for(int a = 0; elment_2[i][a] != '\0'; a++){
+            just[(int)elment_2[i][a]]++;
+        }
     }
-    return ismoil01;
+    for(int param_1 = 0; param_1 < 128; param_1++){
+        if(just[param_1] > 0) {
+            printf("%c:%d\n", param_1, just[param_1]);
+        }
+    }
 }
- 
-  void ziko(int* array,  char * str) {
-       int ismoil01 =0;
-
-     
-       while (str[ismoil01] !='\0') {
-           if(str[ismoil01] != '"'){
-           array[(int)str[ismoil01]] +=1;
-           }
-        ismoil01 += 1;
-       }
-  }
-
-  void print_array(int* array, int size_array) {
-      int ismoil01 = 0;
-
-
-       while(ismoil01 < size_array) {
-           if (array[ismoil01] > 0) {
-             printf ("%c:%d\n", ismoil01, array[ismoil01]);
-           }
-            ismoil01 += 1;
-       }
-  }
-
-  int main(int ac ,char **av) {
-      int ismoil01 = 1;
-      int array[MAX_ARRAY_SIZE] = {0};
-
-
-
-      while (ismoil01 < ac) {
-          ziko(&array[0], av [ismoil01]);
-          ismoil01 +=1;
-      }
-      print_array(&array[0], MAX_ARRAY_SIZE);
-      return EXIT_SUCCESS;
-  }
