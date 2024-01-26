03aa583f81e6029b02a8374205ae5aa9fc487f27
Istamovashakhzoda <istamovashahzoda@gmail.com>
2024-01-26 10:31:02 +0500

    git fixed problems

--- main.c
+++ main.c
@@ -1,16 +1,42 @@
-#include<stdio.h>
-#include<string.h>
+// #include <stdio.h>
+// #include <string.h>
 
-int main() {
-    int ngram[128] = {0};  
+// void my_ngram(char* obc1[], int length) {
+//    int str[127] = {0};
+//    for (int i = 0; i < length; i++) {
+//       for (int j = 0; j < strlen(obc1[i]); j++) {
+//          char word = obc1[i][j];
+//          str[(int)word]++;
+//       }
+//    }
+//    for (int i = 0; i < 127; i++) {
+//       if (str[i] > 0) {
+//          printf("%c: %d\n", 97, str[97]);
+//       }
+//    }
+// }
+// int main(int argc, char* argv[]) {
+//    my_ngram(argv , argc);
+//    return 0;
+// }
 
-    const char* str = "aabbccddff";
-
-    for (int i = 0; i < strlen(str); i++) {
-        ngram[(int)str[i]]++;  
-    }
-
-    printf("%c, %d\n", 'a', ngram['a']);  
-
-    return 0;
+#include <stdio.h>
+#include <string.h>
+void my_ngram(char** obc1, int length) {
+   int str[128] = {0};
+   for (int i = 0; i < length; i++) {
+      for (int j = 0; j < obc1[i][j]; j++) {
+         char word = obc1[i][j];
+         str[(int)word]++;
+      }
+   }
+   for (int i = 0; i < 128; i++) {
+      if (str[i] > 0) {
+         printf("%c: %d\n", i, str[i]);
+      }
+   }
+}
+int main(int argc, char** argv) {
+   my_ngram(argv, argc);
+   return 0;
 }
