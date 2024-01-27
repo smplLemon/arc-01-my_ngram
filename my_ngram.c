f8ed4542a26aa8d4dee089f14b3ac3911a57d13c
boburergashew <153498795+boburergashew@users.noreply.github.com>
2024-01-27 16:32:46 +0500

    Update my_ngram.c

--- my_ngram.c
+++ my_ngram.c
@@ -1,87 +1,14 @@
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
- 
-
-char *sort_str(char *str)
-{
-    char *nstr;
-    nstr = (char *)malloc(strlen(str) - 1);
-    nstr = strdup(str);
-    int strln = strlen(str);
-    char temp;
- 
-    int i, j;
- 
-    for (i = 0; i < strln; i++)
-    {
-        for (j = 0; j < strln; j++)
-        {
-            if (nstr[i] < nstr[j])
-            {
-                temp = nstr[i];
-                nstr[i] = nstr[j];
-                nstr[j] = temp;
-            }
+#include <stdio.h> 
+int main(int elment_1, char* elment_2[]) {
+    int just[128] = {0};
+    for(int i = 1; i < elment_1; i++){
+        for(int a = 0; elment_2[i][a] != '\0'; a++){
+            just[(int)elment_2[i][a]]++;
         }
     }
- 
-    return nstr;
-}
-
-void result(char* word){
-    char *new_str = sort_str(word);
- 
-    int count = 0;
-    char *checked;
-    checked = (char *)malloc(strlen(new_str));
-    int strln = strlen(new_str);
-    for (int i = 0; i < strln; i++)
-    {
-        for (int j = 0; j < strln; j++)
-        {
-            if ((!strchr(checked, new_str[i])) && (new_str[i] == new_str[j]))
-                count++;
+    for(int param_1 = 0; param_1 < 128; param_1++){
+        if(just[param_1] > 0) {
+            printf("%c:%d\n", param_1, just[param_1]);
         }
- 
-        if (!(strchr(checked, new_str[i])))
-        {
-            printf("%c:%d\n", new_str[i], count);
- 
-            count = 0;
-        }
-        checked[i] = new_str[i];
     }
-    free(checked);
-    free(new_str);
-}
- 
-int main(int argc, char **argv)
-{
-    if (argc == 1)
-    {
-    }
-    int i = 1;
-    int j = 1;
-    int totallen = 0;
- 
-    while (1)
-    {
-        if (argv[i] == NULL) break;
-        totallen = totallen + strlen(argv[i]);
-        i++;
-    }
- 
-    char *word = malloc(totallen);
-    while (1)
-    {
-        if (argv[j] == NULL) break;
-        strcat(word, argv[j]);
-        j++;
-    }
-    result(word);
-    
- 
-    return 0;
 }
