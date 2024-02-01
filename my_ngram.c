4d37a071799b37ad60c328918876439c5af35f1d
rinkuo <157690181+rinkuo@users.noreply.github.com>
2024-02-01 16:02:57 +0500

    my_ngram.c

--- my_ngram.c
+++ my_ngram.c
@@ -10,13 +10,15 @@ int my_str(char* str)
     return len;
 }
 
-void pCFreq(char* str) 
+void pCFreq(int argc, char* argv[]) 
 {
     int con[128] = {0};
-    int n = my_str(str);
 
-    for (int i = 0; i < n; i++) {
-        con[(int)str[i]]++;
+    for (int i = 1; i < argc; i++) {
+        int n = my_str(argv[i]);
+        for (int j = 0; j < n; j++) {
+            con[(int)argv[i][j]]++;
+        }
     }
 
     for (int i = 0; i < 128; i++) {
@@ -28,9 +30,6 @@ void pCFreq(char* str)
 
 int main(int argc, char *argv[]) 
 {
-    for (int i = 1; i < argc; i++) {
-        pCFreq(argv[i]);
-    }
-
+    pCFreq(argc, argv);
     return 0;
 }
