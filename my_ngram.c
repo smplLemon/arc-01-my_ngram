538958ee9394ec788630d64c7f0671c12bc15298
unknown <you@example.com>
2024-02-01 17:56:23 +0500

    ngram

--- my_ngram.c
+++ my_ngram.c
@@ -1,26 +1,53 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <string.h>
-#include <stdlib.h>
-int main(int argc, char* argv[]) {
-    int rom[512] = {0};
+#include <stdlib.h> 
+ 
+#define MAX_ARR_SIZE 128 
+ 
+int my_strlen(char* str) 
+{ 
+    int i = 0; 
+    while (str[i] != 0) 
+    { 
+        i += 1; 
+    } 
+    return i; 
+} 
+void arr_fun(int* arr, char* str) 
+{ 
+    int index = 0; 
+    while (str[index] != 0) 
+    { 
+        if (str[index] != '"') 
+        { 
+            arr[(int)str[index]] += 1; 
+        } 
+        index += 1; 
+    } 
+ 
+} 
+void printf_arr(int* arr, int size_arr) 
+{ 
+    int index = 0; 
     
-    int i = 1;
-    while (i < argc) {
-        int a = 0;
-        while (argv[i][a] != 0) {
-            rom[(int)argv[i][a]]++;
-            a++;
-        }
-        i++;
-    }
-    
-    int loop = 0;
-    while (loop < 512) {
-        if (rom[loop] > 0) {
-            printf("%c:%d\n", loop, rom[loop]);
-        }
-        loop++;
-    }
-    
-    return 0;
+    while (index < size_arr) 
+    { 
+        if (arr[index] > 0) 
+        { 
+            printf("%c:%i\n", index, arr[index]); 
+        } 
+        index += 1; 
+    } 
+} 
+int main(int argc, char** argv) 
+{ 
+    int index = 1; 
+    int arr[MAX_ARR_SIZE] = {0}; 
+    while (index < argc) 
+    { 
+        arr_fun(&arr[0], argv[index]); 
+        index += 1; 
+    } 
+    printf_arr(&arr[0], MAX_ARR_SIZE); 
+    return EXIT_SUCCESS; 
 }
