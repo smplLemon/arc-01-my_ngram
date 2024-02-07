715dd289f97d370594408d9b7db710f2c49d0d6c
murodov1 <murodovmirzabek8@gmail.com>
2024-02-07 14:35:13 +0500

    Finished

---
+++ my_ngram.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define SIZE 200
+
+void my_ngram(int ac, char** av)
+{
+    int ch = 0;
+    int j = 0;
+    int count[SIZE] = {0};
+
+    for (int i = 1; i < ac; i++)
+    {
+        for (j = 0; av[i][j] != '\0'; j++)
+        {
+            ch = tolower(av[i][j]);
+            count[(unsigned char)ch]++;
+        }
+    }
+
+    for (int a = 0; a < SIZE; a++)
+    {
+        if (count[a])
+        {
+            printf("%c: %d\n", a, count[a]);
+        }
+    }
+}
+int main(int argc, char **argv)
+{
+    my_ngram(argc, argv);
+    return 0;
+}
