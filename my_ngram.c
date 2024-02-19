e01abcbf0a6f02106c877a8c085fbc5c854eb44a
muxammad0 <157514419+muxammad0@users.noreply.github.com>
2024-02-19 20:26:44 +0500

    Update my_ngram.c

--- my_ngram.c
+++ my_ngram.c
@@ -1,33 +1,14 @@
 #include <stdio.h> 
- 
-#define ASCII_RANGE 128 
- 
-void countCharacters(int argc, char **argv) 
-{ 
-    int counts[ASCII_RANGE] = {0}; 
- 
-    for (int i = 1; i < argc; i++) 
-    { 
-        char *currentArg = argv[i]; 
-         
-        for (int j = 0; currentArg[j] != '\0'; j++) 
-        { 
-            unsigned char currentChar = (unsigned char)currentArg[j]; 
-            counts[currentChar]++; 
-        } 
-    } 
- 
-    for (int i = 0; i < ASCII_RANGE; i++) 
-    { 
-        if (counts[i] > 0) 
-        { 
-            printf("%c: %d\n", i, counts[i]); 
-        } 
-    } 
-} 
- 
-int main(int argc, char **argv) 
-{ 
-    countCharacters(argc, argv); 
-    return 0; 
+int main(int elment_1, char* elment_2[]) {
+    int just[128] = {0};
+    for(int i = 1; i < elment_1; i++){
+        for(int a = 0; elment_2[i][a] != '\0'; a++){
+            just[(int)elment_2[i][a]]++;
+        }
+    }
+    for(int param_1 = 0; param_1 < 128; param_1++){
+        if(just[param_1] > 0) {
+            printf("%c:%d\n", param_1, just[param_1]);
+        }
+    }
 }
