cbd51e952357183f1f1496e127201beeae74c18e
Komiljonov_komronbek <157817727+assalomual@users.noreply.github.com>
2024-02-20 16:19:22 +0300

    Update My_ngram.c

--- My_ngram.c
+++ My_ngram.c
@@ -3,26 +3,27 @@
 
 void countNGrams(char *text) {
     int n = strlen(text);
-    if (n == 0) {
+    if (n ==  0) {
         printf("Matn bo'sh\n");
         return;
     }
 
-    int nGramCounts[256] = {0}; // 256 ta belgilar uchun n-gramlar sonini saqlash uchun massiv
-    for (int i = 0; i < n; i++) {
+    int nGramCounts[256] = {0};
+    for (int i =  0; i < n; i++) {
         char ch = text[i];
         nGramCounts[ch]++;
     }
 
-    for (int i = 0; i < 256; i++) {
-        if (nGramCounts[i] > 0) {
+    for (int i =  0; i <  256; i++) {
+        if (nGramCounts[i] >  0) {
             printf("%c:%d\n", (char)i, nGramCounts[i]);
         }
     }
 }
 
-int main(int argc, char *argv[]) { 
+int main(int argc, char *argv[]) {  
+    char text[256]; 
     printf("Matnni kiriting: ");
-    scanf("%s", argv);
-    countNGrams(argv);
+    scanf("%s", text); 
+    countNGrams(text);
 }
