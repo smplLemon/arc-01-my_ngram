827a617c68e70f16305f3802d54cdacc9b61dcf0
DilnozaNorkulova <158044090+DilnozaNorkulova@users.noreply.github.com>
2024-03-01 18:24:10 +0300

    Update my_ngram.c

--- my_ngram.c
+++ my_ngram.c
@@ -1,26 +1,23 @@
- #include<stdio.h>
+#include<stdio.h>
  #include<string.h>
 
-void c_occurrences( char *text){
-	int i;
-	int k[128] = {0};//ASCII table characters
-
-    for (i=0; text[i]!='\0'; i++){
-	   k[( unsigned char)text[i]]++;
-   }	
+void c_occurrences(int length, char **text){
+  int i, j;
+  int k[128] = {0};//ASCII table characters
+    for(i=1; i<length;i++){
+       for (j=0; text[i][j]; j++){
+        char lt= text[i][j];
+     k[(int)lt]++;
+       }
+   }  
    for(int i=0; i<128; i++){
-	 if(k[i]>0){
-		printf("%c: %d \n",i,k[i]);
-	 }
+   if(k[i]>0){
+    printf("%c: %d \n",i,k[i]);
+   }
    }
 
 }
-int main(int argc, char *argv[]){
-    if(argc < 1){
-       printf( "Usage:%s <text> \n", argv[0]);
-    return 1;  
-    }
-       c_occurrences(argv[1]);
-        printf("\n");
+int main(int argc, char **argv){
+    c_occurrences(argc,argv);
     return 0;
-} 
+}
