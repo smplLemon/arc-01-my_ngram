03d46790805985f1f795c92e4cae3a9b622f8b35
DilnozaNorkulova <158044090+DilnozaNorkulova@users.noreply.github.com>
2024-03-01 16:58:36 +0300

    Update my_ngram.c

--- my_ngram.c
+++ my_ngram.c
@@ -17,7 +17,7 @@ void c_occurrences( char *text){
 }
 int main(int argc, char *argv[]){
     if(argc < 1){
-       printf( "%s \n", argv[0]);
+       printf( "Usage:%s <text> \n", argv[0]);
     return 1;  
     }
        c_occurrences(argv[1]);
